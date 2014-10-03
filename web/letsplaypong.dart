f0ef8f4041d5bf82f0c4e0a64f81f6a00a41d8e4
moru <kevin.singer@student.tuwien.ac.at>
2014-10-04 00:57:42 +0200

    using smartcanvas for svg, so no need any more for dart2js-interfacing with raffael.

--- web/letsplaypong.dart
+++ web/letsplaypong.dart
@@ -1,27 +1,74 @@
-import 'dart:html';
-import 'dart:svg';
+import 'dart:html' as dom hide Text;
+//import 'dart:svg';
+import 'dart:js';
+import 'package:smartcanvas/smartcanvas.dart';
+
 
 
 void main() {
-  Element pongCourt = querySelector("#pongCourt");
-  RectElement barA = new RectElement();
+  dom.Element pongCourt = dom.document.querySelector("#pongCourt");
+  //RectElement barA = new RectElement();
   //barA.height = 50;
   //barA.width = 50;
-  pongCourt.append(barA);
-  
-  
-  querySelector("#sample_text_id")
+  //pongCourt.append(barA);
+
+
+  dom.document.querySelector("#sample_text_id")
       ..text = "Click me!"
       ..onClick.listen(reverseText);
-  
+
+  //TODO write wrapper for Raffael
+  // should be simply importable
+  // or look into dart svg
+
+
+  Rect rect = new Rect({
+    WIDTH: 100,
+    HEIGHT: 100,
+    FILL: 'red',
+    OFFSET_X: -100,
+    OFFSET_Y: -100
+  });
+
+  dom.Element svgContainer = dom.document.querySelector('.svg-canvas');
+  Stage svgStage = new Stage(svgContainer, svg, {});
+
+  //dom.Element canvasContainer = dom.document.querySelector('.canvas-canvas');
+  //Stage canvasStage = new Stage(canvasContainer, canvas, {});
+
+  //var shapes = dom.document.querySelectorAll('input[type=checkbox]:checked');
+
+  //shapes.forEach((shape) {
+  var node, svgNode, canvasNode;
+
+  node = rect;
+  svgNode = node.clone({
+    DRAGGABLE: true
+  });
+  svgNode.on(MOUSEDOWN, (e) {
+    svgNode.moveToTop();
+  });
+  svgStage.addChild(svgNode);
+
+
   //querySelector("#playerA")
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector("#sample_text_id").text;
+void reverseText(dom.MouseEvent event) {
+  var text = dom.document.querySelector("#sample_text_id").text;
   var buffer = new StringBuffer();
   for (int i = text.length - 1; i >= 0; i--) {
     buffer.write(text[i]);
   }
-  querySelector("#sample_text_id").text = buffer.toString();
+  dom.document.querySelector("#sample_text_id").text = buffer.toString();
+
+  
+  // BEGIN Dart2Js
+  print("");
+  print("printing js-helloStr in dart: ");
+  print(context['helloStr']);
+  print('');
+  context.callMethod('sayHello');
+  context.callMethod('myPrint', ['some args to be printed']);
+  // END Dart2Js
 }
