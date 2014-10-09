7822a2a897425ad921eb52e4e12b7abc35d66b02
moru <kevin.singer@student.tuwien.ac.at>
2014-10-09 21:21:56 +0200

    Finished board setup. Players can move with W/S.

--- web/letsplaypong.dart
+++ web/letsplaypong.dart
@@ -1,57 +1,76 @@
 import 'dart:html' as dom hide Text;
-//import 'dart:svg';
+import 'dart:svg' as ds;
 import 'dart:js';
-import 'package:smartcanvas/smartcanvas.dart';
+//import 'package:smartcanvas/smartcanvas.dart';
 
 
 
 void main() {
-  dom.Element pongCourt = dom.document.querySelector("#pongCourt");
-  //RectElement barA = new RectElement();
-  //barA.height = 50;
-  //barA.width = 50;
-  //pongCourt.append(barA);
-
-
+  
+  // INIT
+  
   dom.document.querySelector("#sample_text_id")
-      ..text = "Click me!"
-      ..onClick.listen(reverseText);
+        ..text = "Click me!"
+        ..onClick.listen(reverseText);
+  
+  //dom.Element pongCourt = dom.document.querySelector("#pongCourt");
+  
+  dom.Element pongCourtE = dom.document.querySelector('.svg-canvas');
+  ds.SvgSvgElement pongCourt = (pongCourtE as ds.SvgSvgElement);
+  
+  
+  ds.RectElement leftPlayer = createPlayer(0, 119, 115); //w == 119, s == 115
+  
+  int rightX = pongCourt.clientWidth - 20; //TODO hacky hardcoding
+  ds.RectElement rightPlayer = createPlayer(rightX, 119, 115); //w == 119, s == 115
+  
+  ds.CircleElement ball = new ds.CircleElement()
+    ..attributes['r'] = '24'
+    ..attributes['cx'] = '100'
+    ..attributes['cy'] = '100';
+  
+  pongCourt.children
+    ..add(leftPlayer)
+    ..add(rightPlayer)
+    ..add(ball);
+  
+  // INIT END
+  
+  
+  // MAIN LOOP
+  
+  while(true) {
+    
+  }
+  
+  
+}
 
-  //TODO write wrapper for Raffael
-  // should be simply importable
-  // or look into dart svg
 
 
-  Rect rect = new Rect({
-    WIDTH: 100,
-    HEIGHT: 100,
-    FILL: 'red',
-    OFFSET_X: -100,
-    OFFSET_Y: -100
+ds.RectElement createPlayer(int x, int upKey, int downKey) {
+  for (String side in ['left', 'right']) {
+    print(side);
+  }
+  ds.RectElement player = new ds.RectElement()
+    ..attributes['width'] = '20'
+    ..attributes['height'] = '110'
+    ..attributes['y'] = '10'
+    ..attributes['x'] = '$x';
+  
+  dom.window.onKeyPress.listen((dom.KeyboardEvent e) {
+    int yMove = 0;
+    if(e.keyCode == upKey) {
+      yMove = -10;
+    //} else if (e.keyCode == dom.KeyCode.S) {
+    } else if (e.keyCode == downKey) {
+      yMove = 10;
+    }
+    int y = int.parse(player.attributes['y']) + yMove;
+    player.attributes['y'] = '$y';
   });
-
-  dom.Element svgContainer = dom.document.querySelector('.svg-canvas');
-  Stage svgStage = new Stage(svgContainer, svg, {});
-
-  //dom.Element canvasContainer = dom.document.querySelector('.canvas-canvas');
-  //Stage canvasStage = new Stage(canvasContainer, canvas, {});
-
-  //var shapes = dom.document.querySelectorAll('input[type=checkbox]:checked');
-
-  //shapes.forEach((shape) {
-  var node, svgNode, canvasNode;
-
-  node = rect;
-  svgNode = node.clone({
-    DRAGGABLE: true
-  });
-  svgNode.on(MOUSEDOWN, (e) {
-    svgNode.moveToTop();
-  });
-  svgStage.addChild(svgNode);
-
-
-  //querySelector("#playerA")
+  
+  return player;
 }
 
 void reverseText(dom.MouseEvent event) {
